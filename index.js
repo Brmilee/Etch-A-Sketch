031efc63aedbb235ef4acae7196e90af6d0faa22
Brandon Lee <135774657+Brmilee@users.noreply.github.com>
2025-11-20 14:38:05 -0800

    feat: add hover color effect to grid

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const container = document.querySelector(".container");
 // create array of boxes
 
 function grid(){
-    let gridSize =16
+    let gridSize = 16
     for(let i = 0; i < gridSize; i++){
         for(let j = 0; j < gridSize; j++){
             const div = document.createElement("div");
+            div.addEventListener("mouseover", ()=> {
+                div.setAttribute("style", "background-color: blue;");
+            });
             container.appendChild(div);
         }
     }
 }
 
 grid();
+
+// //mouseover can work as a hover
+// container.div.addEventListener("mouseover", () => {
+//     div.setAttribute("style", "background-color: blue;");
+// });
